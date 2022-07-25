b394b124d624ad62df3fdbb7062c6796681371cc
Vladislav Milevsky <vlad_milev@mail.ru>
2022-07-25 03:12:10 +0000

    add gitlab tf files

---
+++ terraform/runner.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "runner" {
+  name     = "runner"
+  hostname = "runner.milevsky.quest"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd82re2tpfl4chaupeuf" //Ubuntu 20.04 LTS
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
